49531b03c4bb77a32c8545225adf5d2beb6d6956
kurwa <kurwa@HOME-PC>
2023-10-08 19:42:31 +0300

    Добавьте файлы проекта.

---
+++ first/first.cpp
@@ -0,0 +1,11 @@
+﻿//Первая программа
+using namespace std;
+#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Я программирую!" << endl;
+	system("pause");
+	return 0;
+}
+
